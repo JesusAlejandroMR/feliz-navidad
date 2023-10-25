33353393710d2885b339402cabbc2f61c0cd5892
Alejandro Mera <desarrollo1@asiservy.com>
2023-10-25 09:24:14 -0500

    Fix: Cambio en manera de importar createRoot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { createRoot } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './index.css';
