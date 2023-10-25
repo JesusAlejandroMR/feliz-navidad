266809d92b3a91edf6ab69b7424009a084ca53da
JesusAlejandroMR <alejandroje14@hotmail.com>
2023-10-25 09:17:27 -0500

    Fix: test de renderizado singular y comit del git

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React, { useState } from 'react';
+import { createRoot } from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './index.css';
-import Header from '../src/scripts/Header';
-import ListUser from '../src/scripts/ListUser';
-import Identify from '../src/scripts/Identify';
+import Header from './scripts/Header';
+import ListUser from './scripts/ListUser';
+import Identify from './scripts/Identify';
 import reportWebVitals from './reportWebVitals';
 import { DataContextProvider } from './context/dataContext';
 
 const root = document.getElementById('root');
 const reactRoot = createRoot(root);
 
-let cedula = '';
-let usuario = [];
+const App = () => {
+  const [cedula, setCedula] = useState('');
+  const [usuario, setUsuario] = useState([]);
 
-const handleButtonClick = (cedulaValue) => {
-  cedula = cedulaValue;
-  usuario = cedulaValue;
+  const handleButtonClick = (cedulaValue) => {
+    setCedula(cedulaValue);
+    setUsuario(cedulaValue);
+  };
 
-  reactRoot.render(
+  return (
     <React.StrictMode>
       <div className='principal-content'>
         <DataContextProvider>
@@ -37,17 +39,6 @@ const handleButtonClick = (cedulaValue) => {
   );
 };
 
-reactRoot.render(
-  <React.StrictMode>
-    <div className='principal-content'>
-      <DataContextProvider>
-        <div className='header'>
-          <Header usuario={usuario} />
-        </div>
-        <Identify onButtonClick={handleButtonClick} />
-      </DataContextProvider>
-    </div>
-  </React.StrictMode>
-);
+reactRoot.render(<App />);
 
 reportWebVitals();
